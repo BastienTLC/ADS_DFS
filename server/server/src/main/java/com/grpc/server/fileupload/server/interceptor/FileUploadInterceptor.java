25191febbc1c55bde5c8e4daab777e69f8db9880
BastienTLC <bastientalec@orange.fr>
2024-10-10 18:42:46 +0200

    refacto intercepter, let pass request without metadata

--- server/server/src/main/java/com/grpc/server/fileupload/server/interceptor/FileUploadInterceptor.java
+++ server/server/src/main/java/com/grpc/server/fileupload/server/interceptor/FileUploadInterceptor.java
@@ -4,30 +4,38 @@ import com.devProblems.FileMetadata;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.shared.proto.Constants;
 import io.grpc.*;
-import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
+        import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
 
 @GrpcGlobalServerInterceptor
 public class FileUploadInterceptor implements ServerInterceptor {
     @Override
-    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata, ServerCallHandler<ReqT, RespT> serverCallHandler) {
-        FileMetadata fileMetadata = null;
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+            ServerCall<ReqT, RespT> serverCall,
+            Metadata metadata,
+            ServerCallHandler<ReqT, RespT> serverCallHandler) {
+
         if (metadata.containsKey(Constants.fileMetadataKey)) {
             byte[] metaBytes = metadata.get(Constants.fileMetadataKey);
+            FileMetadata fileMetadata;
             try {
                 // here we create the file metadata from the array that we got
                 fileMetadata = FileMetadata.parseFrom(metaBytes);
             } catch (InvalidProtocolBufferException e) {
-                Status status = Status.INTERNAL.withDescription("unable to create file metadata");
+                Status status = Status.INTERNAL.withDescription("Unable to create file metadata");
                 serverCall.close(status, metadata);
+                return new ServerCall.Listener<ReqT>() {};
             }
             // intializing the context that will help us get the value of filemetadata in the service
             Context context = Context.current().withValue(
                     Constants.fileMetaContext,
                     fileMetadata
             );
+            // intercepting the call and adding the context to it
             return Contexts.interceptCall(context, serverCall, metadata, serverCallHandler);
+        } else {
+            // if the metadata does not contain the filemetadata key, we just start the call
+            return serverCallHandler.startCall(serverCall, metadata);
         }
-        return new ServerCall.Listener<>() {
-        };
     }
 }
+
