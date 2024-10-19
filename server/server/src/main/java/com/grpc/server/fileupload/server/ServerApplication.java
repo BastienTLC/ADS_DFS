ba96a0bf133703f5ecf263a8ac584c5c0439bc26
BastienTLC <bastientalec@orange.fr>
2024-10-19 11:51:19 +0200

    create node startup with different argument

--- server/server/src/main/java/com/grpc/server/fileupload/server/ServerApplication.java
+++ server/server/src/main/java/com/grpc/server/fileupload/server/ServerApplication.java
@@ -2,53 +2,76 @@ package com.grpc.server.fileupload.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.MalformedURLException;
-import java.net.UnknownHostException;
-import java.security.Provider;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import java.net.ServerSocket;
-
-import de.uniba.wiai.lspi.chord.com.Endpoint;
-import de.uniba.wiai.lspi.chord.com.Proxy;
-import de.uniba.wiai.lspi.chord.data.*;
+import de.uniba.wiai.lspi.chord.data.URL;
 import de.uniba.wiai.lspi.chord.service.Chord;
-import de.uniba.wiai.lspi.chord.service.ServiceException;
 import de.uniba.wiai.lspi.chord.service.PropertiesLoader;
+import de.uniba.wiai.lspi.chord.service.ServiceException;
 import de.uniba.wiai.lspi.chord.service.impl.ChordImpl;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		//generate random port between 9000 and 9999
-		int grpcPort = generateRandomPort();
+		boolean isBootstrapNode = false;
+		String bootstrapAddress = null;
+
+		// Parse command-line arguments
+		//if it is a bootstrap node, then it will be the first node in the network
+		//if it is not a bootstrap node, then it will join the network so specify the bootstrap node address
+		for (String arg : args) {
+			if (arg.equalsIgnoreCase("bootstrap")) {
+				isBootstrapNode = true;
+			} else if (arg.startsWith("bootstrapAddress=")) {
+				bootstrapAddress = arg.substring("bootstrapAddress=".length());
+			}
+		}
+
+		// Determine host address
+		String host = "localhost"; // Default host
+		try {
+			host = InetAddress.getLocalHost().getHostAddress();
+		} catch (Exception e) {
+			System.err.println("Could not determine local host address, using 'localhost'");
+		}
+
+		// Find available ports
+		int chordPort = findAvailablePort(8000, 9000);
+		int grpcPort = findAvailablePort(9001, 10000);
+
+		// Start Spring Boot application with custom properties
 		SpringApplication app = new SpringApplication(ServerApplication.class);
 		Map<String, Object> properties = new HashMap<>();
-		properties.put("server.port", (grpcPort+1));
+		properties.put("server.port", grpcPort + 1); // For HTTP server if needed
 		properties.put("grpc.server.port", grpcPort);
 		app.setDefaultProperties(properties);
 		app.run(args);
-		//register server to the bootstrap service
-		// registerServer(grpcPort);
-		Chord chord = initializeChordNetwork("localhost", 8080); // this will only be done for the first node
+
+		Chord chord;
+		if (isBootstrapNode) {
+			chord = initializeChordNetwork(host, chordPort);
+		} else {
+			if (bootstrapAddress == null) {
+				System.err.println("Bootstrap address must be provided to join an existing Chord network.");
+				System.exit(1);
+			}
+			chord = joinChordNetwork(host, chordPort, bootstrapAddress);
+		}
 	}
 
-	// Chord functions, these can be broken into classes later
-
-	// this is only done by the first node
+	// Initialize the Chord network (only for the first node)
 	public static Chord initializeChordNetwork(String host, int port) {
-		PropertiesLoader.loadPropertyFile(); // have not configured the properties file
+		PropertiesLoader.loadPropertyFile(); // Ensure Chord properties are loaded
 
 		String protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
-		URL localURL = null;
+		URL localURL;
 		try {
 			localURL = new URL(protocol + "://" + host + ":" + port + "/");
 		} catch (MalformedURLException e) {
@@ -66,55 +89,47 @@ public class ServerApplication {
 		return chord;
 	}
 
-	public static Chord joinChordNetwork(String host, int port, String bootstrapHost, String bootstrapPort) {
+	// Join an existing Chord network
+	public static Chord joinChordNetwork(String host, int port, String bootstrapAddress) {
 		PropertiesLoader.loadPropertyFile();
 		String protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
 
-		URL localURL = null;
+		URL localURL;
 		try {
 			localURL = new URL(protocol + "://" + host + ":" + port + "/");
 		} catch (MalformedURLException e) {
 			throw new RuntimeException("Invalid URL format for Chord node.", e);
 		}
 
-		URL bootstrapURL = null;
+		URL bootstrapURL;
 		try {
-			bootstrapURL = new URL(protocol + "://" + bootstrapHost + ":" + bootstrapPort + "/");
+			bootstrapURL = new URL(protocol + "://" + bootstrapAddress + "/");
 		} catch (MalformedURLException e) {
 			throw new RuntimeException("Invalid URL of bootstrap node", e);
 		}
 
 		Chord chord = new ChordImpl();
-		try{
+		try {
 			chord.join(localURL, bootstrapURL);
-			System.out.println("Chord network joined at: " + bootstrapURL);
+			System.out.println("Joined Chord network via bootstrap node at: " + bootstrapURL);
 		} catch (ServiceException e) {
 			throw new RuntimeException("Could not join Chord network!", e);
 		}
 
 		return chord;
-
-
 	}
 
-	private static void registerServer(int grpcPort) {
-		RestTemplate restTemplate = new RestTemplate();
-		String bootstrapUrl = "http://127.0.0.1:8085/addServer";
-
-		try {
-			String serverIp = InetAddress.getLocalHost().getHostAddress();
-			String url = String.format("%s?ip=%s&port=%d", bootstrapUrl, serverIp, grpcPort);
-			restTemplate.postForObject(url, null, String.class);
-		} catch (UnknownHostException e) {
-			System.out.println("Error while registering server to the bootstrap service: " + e.getMessage());
-		} catch (Exception e) {
-			System.out.println("Error while registering server to the bootstrap service: " + e.getMessage());
+	// Method to find an available port within a given range
+	private static int findAvailablePort(int minPort, int maxPort) {
+		for (int port = minPort; port <= maxPort; port++) {
+			if (isPortAvailable(port)) {
+				return port;
+			}
 		}
+		throw new RuntimeException("No available port found in the range " + minPort + "-" + maxPort);
 	}
 
-
-
-
+	// Check if a port is available
 	private static boolean isPortAvailable(int port) {
 		try (ServerSocket socket = new ServerSocket(port)) {
 			socket.setReuseAddress(true);
@@ -124,13 +139,10 @@ public class ServerApplication {
 		}
 	}
 
-	// generates random port and makes sure that it is available
-	private static int generateRandomPort() {
-		int port;
-		do {
-			port = (int) (Math.random() * 1000) + 9000;
-		} while (!isPortAvailable(port));
-		return port;
+	@Bean
+	public Chord chord() {
+		Chord chord = new ChordImpl();
+		// Initialize the Chord instance as needed
+		return chord;
 	}
-
 }
